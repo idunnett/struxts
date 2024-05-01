ca3839ff8d031a39d9357d497ade0379ad811731
Isaac Dunnett <isaacsdunnett@gmail.com>
2024-05-01 13:57:59 -0300

    updated sign in redirect url

--- src/app/sign-in/[[...rest]]/page.tsx
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <SignIn routing="hash" />
+      <SignIn routing="hash" fallbackRedirectUrl="/structures" />
     </div>
   )
 }
